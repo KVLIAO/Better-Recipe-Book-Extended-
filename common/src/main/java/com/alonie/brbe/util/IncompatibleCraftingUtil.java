d814e466871d0b6e6a0040c4fdc8730e21b52186
Avalonia <avalonia@users.noreply.github.com>
2026-06-09 16:01:50 +0800

    Fix: remove duplicate else block

--- common/src/main/java/com/alonie/brbe/util/IncompatibleCraftingUtil.java
+++ common/src/main/java/com/alonie/brbe/util/IncompatibleCraftingUtil.java
@@ -58,9 +58,6 @@ public final class IncompatibleCraftingUtil {
             } else {
                 if (incompatible == null) incompatible = new HashSet<>();
                 incompatible.add(entry.id());
-            } else {
-                if (incompatible == null) incompatible = new HashSet<>();
-                incompatible.add(entry.id());
             }
         }
 
