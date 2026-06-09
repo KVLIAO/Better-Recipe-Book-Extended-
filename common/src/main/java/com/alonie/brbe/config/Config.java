bebc551796a1f7331f304367bd5c5cd68b667975
Avalonia <avalonia@users.noreply.github.com>
2026-06-09 13:09:37 +0800

    Remove tooltip from showAllRecipesInSurvival (1.21.1)

--- common/src/main/java/com/alonie/brbe/config/Config.java
+++ common/src/main/java/com/alonie/brbe/config/Config.java
@@ -19,7 +19,6 @@ public class Config implements ConfigData {
     @ConfigEntry.Gui.Tooltip()
     public boolean hideReiJeiOverlay = false;
 
-    @ConfigEntry.Gui.Tooltip()
     public boolean showAllRecipesInSurvival = true;
 
     @ConfigEntry.Category("newRecipes")
